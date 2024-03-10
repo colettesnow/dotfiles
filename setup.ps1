d5aa3e6600c59399dd0b4cddd584ba3559b0a70f
Colette Snow <colette@colettesnow.com>
2024-03-11 09:04:51 +1100

    Expand on setup powershell script

--- setup.ps1
+++ setup.ps1
@@ -1,33 +1,67 @@
 ## This script is in not complete. Use at own risk.
 
 ## Install Scoop
-Set-ExecutionPolicy RemoteSigned -Scope CurrentUser # Optional: Needed to run a remote script the first time
-irm get.scoop.sh | iex
+if (Test-Path -Path "$HOME/scoop") {
+    Write-Output "Scoop is already installed. Continuing…"
+} else {
+    Set-ExecutionPolicy RemoteSigned -Scope CurrentUser # Optional: Needed to run a remote script the first time
+    Invoke-RestMethod get.scoop.sh | Invoke-Expression
+}
 
-scoop install sudo nano
+$scoop_apps = "sudo", "nano", "grep"
+$choco_apps = "filezilla", "mupdf"
+$winget_categories = "base", "audiovideo", "comm", "dev", "graphics", "games", "productivity", "security", "utils", "browsers"
+$winget_app_pins = "Valve.Steam", "ElectronicArts.EADesktop", "GOG.Galaxy", "Ubisoft.Connect", "Amazon.Games", "EpicGames.EpicGamesLauncher"
 
-## Install Chocolately
-sudo Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+# Install Apps
 
-sudo choco install 7zip kdenlive filezilla php -y
+foreach ($app in $scoop_apps)
+{
+    Write-Output "Installing $app using Scoop."
+    scoop install $app
+}
 
-# Install Apps using Winget
+Write-Output "You will receive several user account control prompts, please allow them promptly."
+read-host "Press Enter to Continue"
 
-winget import -i winget/base.json --accept-package-agreements --accept-source-agreements --ignore-unavailable
-winget import -i winget/audiovideo.json --accept-package-agreements --accept-source-agreements --ignore-unavailable
-winget import -i winget/comm.json --accept-package-agreements --accept-source-agreements --ignore-unavailable
-winget import -i winget/dev.json --accept-package-agreements --accept-source-agreements --ignore-unavailable
-winget import -i winget/graphics.json --accept-package-agreements --accept-source-agreements --ignore-unavailable
-winget import -i winget/games.json --accept-package-agreements --accept-source-agreements --ignore-unavailable
-winget import -i winget/productivity.json --accept-package-agreements --accept-source-agreements --ignore-unavailable
-winget import -i winget/security.json --accept-package-agreements --accept-source-agreements --ignore-unavailable
-winget import -i winget/utils.json --accept-package-agreements --accept-source-agreements --ignore-unavailable
-winget import -i winget/browsers.json --accept-package-agreements --accept-source-agreements --ignore-unavailable
+if (Test-Path -Path "C:\ProgramData\chocolatey") {
+    Write-Output "Chocolately is already installed. Continuing…"
+} else {
+    ## Install Chocolately
+    sudo Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+}
+
+# Install Choco Apps
+foreach ($app in $choco_apps)
+{
+    Write-Output "Installing $app using Chocolately."
+    sudo choco install $app -y
+}
+
+foreach ($app_category in $winget_categories)
+{
+    Write-Output ""
+    Write-Output "Installing $app_category Apps using Winget"
+    Write-Output "============================================"
+    winget import -i winget/$app_category.json --accept-package-agreements --accept-source-agreements --ignore-unavailable
+}
 
 # Pin Game Launchers since they update themselves
-winget pin add Valve.Steam
-winget pin add ElectronicArts.EADesktop
-winget pin add GOG.Galaxy
-winget pin add Ubisoft.Connect
-winget pin add Amazon.Games
-winget pin add EpicGames.EpicGamesLauncher
+
+foreach ($app in $winget_app_pins)
+{
+    Write-Output "Adding pin for $app."
+    winget pin add $app
+}
+
+# Delete Desktop Shortcuts
+Write-Output "Cleaned up/deleted Application Desktop Shortcuts"
+sudo Remove-Item C:\Users\Public\Desktop\*.lnk
+Remove-Item $HOME\Desktop\*.lnk
+
+# Git
+$user_gitname = Read-Host "Please enter your Full Name for Git"
+$user_gitemail = Read-Host "Please enter your Email Address for Git"
+
+git config --global user.name "$user_gitname"
+git config --global user.email "$user_gitemail"
