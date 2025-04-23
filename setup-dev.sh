b32a8d9e08189f18ed48e60aafe26ff34a1f8552
Colette Snow <colette@colettesnow.com>
2025-04-23 11:35:15 +1000

    Tell shell check to ignore nvm files

--- setup-dev.sh
+++ setup-dev.sh
@@ -10,7 +10,9 @@ sudo apt install eza git git-lfs golang gh build-essential neofetch php-cgi pyth
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 
 export NVM_DIR="$HOME/.nvm"
+# shellcheck source=/dev/null
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+# shellcheck source=/dev/null
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 nvm install node
